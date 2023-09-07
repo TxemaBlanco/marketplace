8171ddcbbeddf3f98111484b7047a8c91050781a
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-09-07 15:15:18 +0200

    Create component banner

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginviewComponent } from './views/loginview/loginview.component';
 import { HomeuserviewComponent } from './views/homeuserview/homeuserview.component';
 import { HomeadminviewComponent } from './views/homeadminview/homeadminview.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginviewComponent,
     HomeuserviewComponent,
-    HomeadminviewComponent
+    HomeadminviewComponent,
+    BannerComponent 
+   
   ],
   imports: [
     BrowserModule,
