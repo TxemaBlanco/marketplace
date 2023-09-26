182cd31ecc93e2aebc0d3d9bf617f8034d769de6
kayyra <anarodriguez.p@hotmail.es>
2023-09-26 08:44:51 +0200

    fixed all

--- front/src/app/services/customer.service.ts
+++ front/src/app/services/customer.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Customer } from '../models/Customer.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
-
-  constructor() { }
+  private customerUrl = 'http://localhost:8000/customers'
+  constructor(private httpClient: HttpClient) { }
+  register(customer : Customer){
+    return this.httpClient.post<Customer>(this.customerUrl, customer);
+  }
 }
