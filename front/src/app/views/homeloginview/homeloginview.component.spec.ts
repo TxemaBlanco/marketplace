29eeb6a601e1ca7a099cf17a1d04dc3e7adbff9f
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-09-27 09:12:42 +0200

    Changes name of component

--- front/src/app/views/homeloginview/homeloginview.component.spec.ts
+++ front/src/app/views/homeloginview/homeloginview.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeadminviewComponent } from './homeadminview.component';
+import { HomeadminviewComponent } from './homeloginview.component';
 
 describe('HomeadminviewComponent', () => {
   let component: HomeadminviewComponent;
