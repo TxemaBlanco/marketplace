1d454b74d1a365d75518f4afa8979b369b60d74d
Ana <anarodriguez.p@hotmail.es>
2023-10-02 08:22:55 +0200

    added comic table admin

---
+++ front/src/app/components/admin/comictableadmin/comictableadmin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comictableadmin',
+  templateUrl: './comictableadmin.component.html',
+  styleUrls: ['./comictableadmin.component.scss']
+})
+export class ComictableadminComponent {
+
+}
