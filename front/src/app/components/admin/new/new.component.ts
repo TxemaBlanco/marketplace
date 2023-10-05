7b7dc3ec43846537d28d95b9e6da519fbde6b154
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-10-05 14:35:47 +0200

    Rrestructurate new component

--- front/src/app/components/admin/new/new.component.ts
+++ front/src/app/components/admin/new/new.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Genre } from 'src/app/models/Genre';
 import { ComicService } from 'src/app/services/comic.service';
@@ -13,29 +13,23 @@ import { HttpEventType, HttpResponse } from '@angular/common/http';
   templateUrl: './new.component.html',
   styleUrls: ['./new.component.scss'],
 })
-export class NewComponent implements OnInit {
+export class NewComponent implements OnInit{
   @ViewChild('formcomics') formcomics!: NgForm;
   myForm: FormGroup;
   selectedFiles?: FileList;
   currentFile?: File;
-  fileName?: any;
+  fileName?:any;
   progress = 0;
   message = '';
   preview = '';
   imageInfos?: Observable<any>;
   genres: Genre[] = [];
   currentGenres: Genre[] = [];
-  currentGenre!: Genre;
+  currentGenre!:Genre ;
   covers: string[] = ['Tapa Dura', 'Tapa Blanda'];
-  @ViewChild('divList')
-  divList!: ElementRef;
-  staticHtml = '<div class="insert"></div>'
 
-  constructor(
-    private comicService: ComicService,
-    private uploadService: FileUploadService,
-    fb: FormBuilder
-  ) {
+  constructor(private comicService: ComicService,private uploadService:FileUploadService, fb: FormBuilder) {
+    
     this.myForm = fb.group({
       isbn: ['', [Validators.required, Validators.pattern('[0-9]{13}')]],
       title: [''],
@@ -57,16 +51,16 @@ export class NewComponent implements OnInit {
       this.genres = genres;
     });
   }
-
-  onSubmit() {
+  
+  onSubmit() {      
     this.upload();
     this.myForm.controls['photo'].setValue(this.fileName);
     if (this.myForm.valid) {
       const comicData = this.myForm.value;
       let genreData = {
-        genres: this.currentGenres,
-      };
-      Object.assign(comicData, genreData);
+        genres:[this.currentGenre]
+      }
+      Object.assign(comicData,genreData);
       console.log(comicData);
       this.comicService.newComic(comicData).subscribe(
         (response: any) => {
@@ -87,43 +81,43 @@ export class NewComponent implements OnInit {
           console.error('Error durante el registro de nuevo comic', error);
         }
       );
-    }
+    }    
   }
-
+  
   selectFile(event: any): void {
     this.message = '';
     this.preview = '';
     this.progress = 0;
     this.selectedFiles = event.target.files;
-
+  
     if (this.selectedFiles) {
       const file: File | null = this.selectedFiles.item(0);
-
+  
       if (file) {
         this.preview = '';
         this.currentFile = file;
-
+  
         const reader = new FileReader();
-
+  
         reader.onload = (e: any) => {
           console.log(e.target.result);
           this.preview = e.target.result;
         };
-
+  
         reader.readAsDataURL(this.currentFile);
       }
     }
   }
   upload(): void {
     this.progress = 0;
-
+  
     if (this.selectedFiles) {
       const file: File | null = this.selectedFiles.item(0);
       this.fileName = file?.name;
-
+  
       if (file) {
         this.currentFile = file;
-
+  
         this.uploadService.upload(this.currentFile).subscribe({
           next: (event: any) => {
             if (event.type === HttpEventType.UploadProgress) {
@@ -136,23 +130,26 @@ export class NewComponent implements OnInit {
           error: (err: any) => {
             console.log(err);
             this.progress = 0;
-
+  
             if (err.error && err.error.message) {
               this.message = err.error.message;
             } else {
               this.message = 'Could not upload the image!';
             }
-
+  
             this.currentFile = undefined;
           },
         });
       }
-
+  
       this.selectedFiles = undefined;
     }
   }
-
-  newGenre(selectedGenre: Genre) {
+  addGenre(genre:Genre){
+    this.genres.push(genre);
+    console.log("añadido = " + genre.name);
+  }
+  newGenre(selectedGenre:Genre){    
     this.currentGenre = selectedGenre;
     // Swal.fire({
     //   icon: 'success',
@@ -161,32 +158,25 @@ export class NewComponent implements OnInit {
     // })
     // console.log(this.currentGenre.name);
   }
-  addGenreToList(genre:Genre) 
-  {
-    this.addGenre(genre);
-  }
-  addGenre(genre: Genre) {
-    this.currentGenres.push(genre);
-    console.log('añadido = ' + genre.name);
-  }
-  deleteGenre(index:number){
-    console.log("function deleteGenre-> index=" + index);
-    this.currentGenres.forEach(currentGenre=>console.log(currentGenre.name));
-    this.currentGenres.splice(index,1);    
-  }
-  renderGenreList(){
-    const div = this.divList.nativeElement.querySelector(".insert");
-    const itemLista = document.createElement('div');
+  editarLista(){
     for (let i = 0; i < this.currentGenres.length; i++) {
-      let genreName = this.currentGenres[i].name;      
-      itemLista.innerHTML =
-        genreName +
-        ' ' +
-        '<button (click)="this.deleteGenre(' +
-        i +
-        ')">Eliminar</button>'; 
-        console.log("genre index"+i);     
-    }
-    div.appendChild(itemLista);
+    // let genreName = currentGenres[i].name;
+
+    // let itemLista = document.createElement("div");
+    // itemLista.innerHTML =
+    //   nombre +
+    //   " " +
+    //   '<button onclick="eliminarJugador(' +
+    //   i +
+    //   ')">Eliminar</button>' +
+    //   '<button onclick="editarJugador(' +
+    //   i +
+    //   ')">Editar</button>';
+
+    // listaJugadores.appendChild(itemLista);
   }
+  }
+  
+  
+  
 }
