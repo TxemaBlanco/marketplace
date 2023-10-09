33b9ed297dc858e0d7e8104d49f0a8935002fda1
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-10-09 12:12:54 +0200

    delete element corrupted

--- front/src/app/components/admin/comictableadmin/comictableadmin.component.spec.ts
+++ front/src/app/components/admin/comictableadmin/comictableadmin.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ComictableadminComponent } from './comictableadmin.component';
 
 describe('ComictableadminComponent', () => {
@@ -8,14 +7,38 @@ describe('ComictableadminComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ComictableadminComponent]
+      declarations: [ComictableadminComponent],
     });
     fixture = TestBed.createComponent(ComictableadminComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize comics and genres on ngOnInit', () => {
+    spyOn(component, 'getComics');
+    spyOn(component, 'getGenres');
+    
+    component.ngOnInit();
+    
+    expect(component.getComics).toHaveBeenCalled();
+    expect(component.getGenres).toHaveBeenCalled();
+  });
+
+  it('should filter comics based on search text', () => {
+    component.comics = [
+      { isbn:'12314523651235' , title: 'Comic 1', author: 'Author 1', genres: 'Juvenil'  }
+   
+    ];
+
+    component.searchTextGlobal = 'Comic 1';
+
+    component.globalsearch();
+
+    expect(component.comics.length).toBe(1);
+    expect(component.comics[0].title).toBe('Comic 1');
+  });
+
 });
