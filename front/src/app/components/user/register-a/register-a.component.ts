4d0218baf88692a410b01772e01ed798e521ae22
JSanLazaro <jsanlazaro@hotmail.com>
2023-10-09 10:56:17 +0200

    filter compras

--- front/src/app/components/user/register-a/register-a.component.ts
+++ front/src/app/components/user/register-a/register-a.component.ts
@@ -41,7 +41,6 @@ export class RegisterAComponent {
   ) {
     this.registrationForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)]],
-      dniType: ['', Validators.required],
       dni: ['', [Validators.required, Validators.pattern('[0-9]{8}[A-Z]')]],
       name: ['', [Validators.required, Validators.pattern('[A-Za-z ]*')]], 
       surname:  ['', [Validators.required, Validators.pattern('[A-Za-z ]*')]], 
